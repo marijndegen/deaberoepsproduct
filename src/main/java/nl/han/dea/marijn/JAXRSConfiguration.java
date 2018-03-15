38fcc5c87e019b049601b5162018f65e81648eb1
Marijn Degen <marijndegen96@gmail.com>
2018-03-15 10:47:50 +0100

    Refactored default namespace

--- src/main/java/nl/han/dea/marijn/JAXRSConfiguration.java
+++ src/main/java/nl/han/dea/marijn/JAXRSConfiguration.java
@@ -1,4 +1,4 @@
-package com.airhacks;
+package nl.han.dea.marijn;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
--- src/main/java/nl/han/dea/marijn/helloworld.java
+++ src/main/java/nl/han/dea/marijn/helloworld.java
@@ -1,4 +1,4 @@
-package com.airhacks;
+package nl.han.dea.marijn;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.Produces;
 import javax.ws.rs.GET;
