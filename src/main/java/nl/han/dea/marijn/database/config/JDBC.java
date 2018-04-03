30989ae0e884758cbb77afdf3b7fe30900351514
Marijn Degen <marijndegen96@gmail.com>
2018-04-03 14:20:21 +0200

    database.properties bestand werkend... (gestest)

--- src/main/java/nl/han/dea/marijn/database/config/JDBC.java
+++ src/main/java/nl/han/dea/marijn/database/config/JDBC.java
@@ -2,10 +2,30 @@ package nl.han.dea.marijn.database.config;
 
 import org.javalite.activejdbc.Base;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class JDBC {
     public static void start(){
-        Config databaseConfig = new Config();
-        Base.open(databaseConfig.getDriver(), databaseConfig.getUrl(), databaseConfig.getUser(), databaseConfig.getPassword());
+        Properties props = new Properties();
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        InputStream input = classLoader.getResourceAsStream("resources.properties");
+        try {
+            props.load(input);
+            input.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String driver   = props.getProperty("jdbc.driver");
+        String url      = props.getProperty("jdbc.url");
+        String username = props.getProperty("jdbc.username");
+        String password = props.getProperty("jdbc.password");
+        System.out.println(driver);
+        System.out.println(url);
+        System.out.println(username);
+        System.out.println(password);
+        Base.open(driver, url, username, password);
     }
 
     public static void stop(){
